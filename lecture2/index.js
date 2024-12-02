b4e2c5f6d7523df42816bcf8693a9313aa7cf508
focus011147 <k.focus321@gmail.com>
2024-12-02 23:35:15 +0700

    lecture2

---
+++ lecture2/index.js
@@ -0,0 +1,11 @@
+const message = "Hello";
+const place  = "World";
+
+// logging out a string
+console.log('Hello,Word!')
+
+// using substitutions
+console.log('hello, %s!',place);
+
+//using a string literal
+console.log('${message},${place}!');
