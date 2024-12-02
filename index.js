ded7ca4ff469ce42587157d6aed4337504f540b7
focus011147 <k.focus321@gmail.com>
2024-12-02 23:35:55 +0700

    lecture2

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-// my first javascript
-console.log(" I did it");
+const message = "Hello";
+const place  = "World";
+//git config --global user.name "focus011147"
+// logging out a string
+console.log('Hello,Word!') // git config --global user.email "k.focus321@gmail.com"
 
+// using substitutions
+console.log('hello, %s!',place);
+
+//using a string literal
+console.log('${message},${place}!');
